921b09b93f4303948b8ce18ebac8f805d2b8e612
nicosalasn <nicosalasn@gmail.com>
2020-04-13 11:27:30 -0400

    mis cambios

--- Class_02/Script_Clase02.R
+++ Class_02/Script_Clase02.R
@@ -18,108 +18,92 @@ x<-NULL #objeto vacío
 y<-c(TRUE,FALSE)#concatenar valores logicos
 as.numeric(y) #convierta un objeto en numerico, si es que puede
 
-A<-1
-<<<<<<< HEAD
+A<-1 #creamos un objeto valor 1
 years<-2010:2020 #objeto que tomara el rango completo del 2010 al 2020
 tiktoc<-c("Que", "linda", "te ves", "limpiando", "Esperancita") #concatena los string entre parentesis, todos los elementos
-class(tiktoc)
-=======
-years<-2010:2020
-year<- seq(2010,2020,by = 0.5)
-tiktoc<-c("Que", "linda", "te ves", "limpiando", "Esperancita")
+class(tiktoc) #busca la clase del parentesis, caracter en este caso
+year<- seq(2010,2020,by = 0.5)#secuencia de numeros de rango 2010 al 2020 con aumento de 0.5 cada vez
 
-paste("Hola","Mundo",sep=" ")
+paste("Hola","Mundo",sep=" ") #Hola mundo separado con un espacio con la función sep
 
-paste(tiktoc,collapse = " ")
+paste(tiktoc,collapse = " ") #collapse es lo mismo que el sep, pero cuando tomamos la función 
 
-obj2<- as.numeric(c(1,2,3,4,"Esperancita"))
-is.na(obj2)
+obj2<- as.numeric(c(1,2,3,4,"Esperancita")) #objeto 2 que contiene si es un numero cierta concatenación, el ultimo es NA, porque no es numero
+is.na(obj2) #busca si hay na en el objeto 2
 
 
-numeros_en_texto<-c("1","2","3")
-as.numeric(numeros_en_texto)
+numeros_en_texto<-c("1","2","3") #nuevo objeto, reconoce estos caracteres como numeros
+as.numeric(numeros_en_texto) #pregunta si son numeros y los muestra para el parentesis
 
->>>>>>> 7a0750334e5bd6a30dc923727a86b4ca42c7af30
-m1<-matrix(1:4,2,2)
-m1%*%t(m1)
-diag(m1)
+m1<-matrix(1:4,2,2) #Nueva matriz de 1 a 4 de 2 x 2
+m1%*%t(m1) #traspuesta de m1, multiplicada por la normal
+diag(m1) #diagonal de la matriz
 solve(m1)
 
 
-a1<-array(1:12,dim = c(2,2,3))
+a1<-array(1:12,dim = c(2,2,3)) #arreglo del 1 al 12, en dimensión 2x2 en 3 matrices
 
-d1<-data.frame(m1)
-data("quakes") # promise
-d1<-data.frame(quakes)
+d1<-data.frame(m1) #crea un data frame con los datos de m1
+data("quakes") # promise en el envirioment
+d1<-data.frame(quakes) #crea un data frame con los datos de los temblores
 
-ls()
+ls() #muestra todo lo que tenemos en el ambiente (Environment)
 
-l1<-list(Perrito=A,years,tiktoc,m1)
-A<-3L
+l1<-list(Perrito=A,years,tiktoc,m1)  #crea unba lista con el contenido entre parentesis
+A<-3L #la L hace que sea entero
 
 # Manipulación de Objetos
-ls()
+A<-1L #la L hace que sea entero
 
-A<-1L
+class(A) #clase del objeto
+typeof(A) #es más general, dice la clase del objeto
 
-class(A)
-typeof(A)
+length(years) #largo de años del objeto
+dim(m1) #dimensión de m1
 
-length(years)
-dim(m1)
+object.size(d1)#el tamaño en bytes de d1
 
-object.size(d1)
+names(d1) #nombre de las columnas de d1
+head(d1) #muestra los primeros 6 de d1
+tail(d1) #muestra los ultimos 6 de d1
 
-names(d1)
-head(d1)
-tail(d1)
-
-rm(A)
+rm(A) #Remueve el objeto
 
 #Bonus: como se borra todo?
-rm(list=ls())
-<<<<<<< HEAD
-=======
+rm(list=ls()) #BORRA TODO LO DEL ENVIRONMENT
 
->>>>>>> 7a0750334e5bd6a30dc923727a86b4ca42c7af30
 # Indexación uso de los []
 
-length(years)
-years[11]
+length(years) #largo de years
+years[11] #posición 11 de largo
 
-dim(m1)
-m1[1,2]
+m1[1,2] #m1 posición 1,2 
 
-dim(a1)
-<<<<<<< HEAD
+dim(a1) #dimension de a1 , 2x2 3 veces
+
 a1[2,1,3] #fila, columna,dimension en ese orden
-=======
-class(a1)
-a1[2,1,3]
->>>>>>> 7a0750334e5bd6a30dc923727a86b4ca42c7af30
 
-l1[2]
-l1[2][[1]][1:2]
+class(a1) #clase del arreglo a1
 
-l1[[2]][3:5]
+l1[2] #l1 lista 1, años
+l1[2][[1]][1:2] #years de l1 indice 1 y 2, el corchete 1 es referente a la fila
 
-l1$Perrito
+l1[[2]][3:5] # pedir el vector 2 de la lista que es el variable years, rango 3,4,5
 
-d1[1,]
-d1[,1]
-d1[,'lat']
-d1$mag[seq(1,16,2)]
-d1$lat[1:4]
+l1$Perrito #llama la variable perrito y muestra su valor
 
-d1[,'lat']
-d1[1:4,c('lat','long')]
+d1[1,] #muesta la primera fila de d1 
+d1[,1] #muestra la primera columna para todas las filas de d1
+d1[,'lat'] #muestra la columna latitud para todas las filas de d1
+d1$mag[seq(1,16,2)] #para d1 busca la variable magnitud con secuencia de 1 al 16 con incremento de 2 en 2
+d1$lat[1:4] #para d1 busca la variable latitud y muestra del 1 al 4
 
-d1$mag>5
-table(d1$mag>5)
-d1[d1$mag>6,'stations']
+d1[1:4,c('lat','long')] # del rango 1 al 4 muestrame latitud y longitud
 
-d1$dummy_5up<-as.numeric(d1$mag>5)
-head(d1)
+d1$mag>5 #operacion logica que devuelve bool si la magnitud de mayores a 5, si se cumple o no
+table(d1$mag>5) #computa en una tabla los valores de la operación anterior, COMO UN CONTADOR
+d1[d1$mag>6,'stations'] #estaciones que han tenido magnitud mayores a 6
+d1$dummy_5up<-as.numeric(d1$mag>5) #crea una nueva variable para ver si se cumple si la magnitud fue mayor a 5
 
 # Distinguir entre funciones, objetos, números y sintaxis básica
 # Funciones: palabra + () con argumentos separados por commas
@@ -128,38 +112,38 @@ head(d1)
 
 #---- Part 2: Loops  -------------------
 
-A<-2
+A<-2 #objeto valor 2
 
 if(A==1){
   print("A es un objeto con un elemento numérico 1")
 } else {
   print("A no es igual a 1, pero no se preocupe que lo hacemos")
   A<-1L
-}
+} #si se cumple la condición, pasa lo siguiente
 
-A<-1
-class(A)
-typeof(A)
+A<-1 #valor 1 al objeto
+class(A) #clase
+typeof(A)#clse general
 
-dim(A)
-length(A)
+dim(A) #null porque no es array ni matriz
+length(A) #largo de objeto
 
 # For loop
 
 for(i in 1:5){
-  print(paste("Me le declaro a la ", i))
-  Sys.sleep(2)
+  print(paste("Me le declaro a la ", i)) #EL PASTE ES PARA QUE TOME LA VARIABLE
+  Sys.sleep(2) #DUERME 2 SEGUNDOS ANTES DE SEGUIR CORRIENDO, SE DUERME LA CONSOLA
   print("no mejor no... fail!")
   Sys.sleep(1)
-}
+} #LOOP de 1 a 5, que se recorre
 
 i<-1
 eps<-50/(i^2)
 while(eps>0.001){
   eps<-50/(i^2)
-  print(paste("eps value es still..", eps))
+  print(paste("eps value es still..", eps)) #CON EL PASTE TOMA LA VARIABLE
   i<-i+1
-}
+} #WHILE VA A RECORRER SIEMPRE QUE SE CUMPLA
 
 #---- Part 3: Data Management ----
 # Tres formas de trabajar con datos
@@ -167,28 +151,28 @@ while(eps>0.001){
 ### 1. R-Base 
 #http://github.com/rstudio/cheatsheets/raw/master/base-r.pdf
 
-quakes[quakes$mag>6,'mag']
+quakes[quakes$mag>6,'mag'] #muestrame las magnitudes que sean mayores a 6
 
-by(data = quakes$mag,INDICES = quakes$stations,FUN = mean)
-tapply(X = quakes$mag,INDEX = quakes$stations, FUN = mean)
+by(data = quakes$mag,INDICES = quakes$stations,FUN = mean) #para la data quakes, la magnitud va a ser indice y estaciones el promedio de las magnitudes
+tapply(X = quakes$mag,INDEX = quakes$stations, FUN = mean) #promedia las magnitudes y los muestra en base a las estaciones
 
 ### 2. tydiverse 
 #https://rstudio.com/wp-content/uploads/2015/02/data-wrangling-cheatsheet.pdf
 library(tidyverse)
 #Cómo se instala el paquete si no lo tengo? Tank!!! ayudaaaa!
-<<<<<<< HEAD
+
 install.packages("tidyverse")
-=======
+
 #install.packages("tydiverse")
->>>>>>> 7a0750334e5bd6a30dc923727a86b4ca42c7af30
+
 
 quakes %>% 
   filter(mag>6) %>% 
-  select(mag) 
+  select(mag)  #con tydiverse buscame y filtrame las magnitudes mayores a 6
 
 quakes %>% 
   group_by(stations) %>%
-  summarise(mean(mag))
+  summarise(mean(mag)) #agrupame el resumen del promedio de las magnitudes en base a las estaciones correspondientes
 
 
 ### 3. data.table (recommended in this course)
@@ -196,24 +180,24 @@ install.packages("data.table")
 library(data.table)
 #https://github.com/rstudio/cheatsheets/raw/master/datatable.pdf
 #install.packages("data.table")
-quakes<-data.table(quakes)
+quakes<-data.table(quakes) #crea quakes como data table
 
 
-quakes[quakes$mag>6,'mag']
+quakes[quakes$mag>6,'mag'] #para quakes busca que se cumpla la condición cuando la variable mag es mayor a 6 y muestramelos en data table 
 
-quakes[mag>6,.(mag)]
+quakes[mag>6,.(mag)] #Para quakes, busca cundo se cumpla que magnitud mayor a 6, en base a mag... EL , ES PARA LA FUNCION Y EL . ES PARA SEPARAR HACIA ABAJO
 
-quakes[,mean(mag),by=.(stations)]
+quakes[,mean(mag),by=.(stations)]# para quakes busca el promedio de magnitud y ordenalo por estaciones
 
 ### Reading data from a file
 
-library(readxl)
+library(readxl) #carga el abrir excel
 
-casos<-data.table(read_excel("Class_02/2020-03-17-Casos-confirmados.xlsx",na = "—",trim_ws = TRUE,col_names = TRUE),stringsAsFactors = FALSE)
+casos<-data.table(read_excel("Class_02/2020-03-17-Casos-confirmados.xlsx",na = "—",trim_ws = TRUE,col_names = TRUE),stringsAsFactors = FALSE) #abreme el archivo, remplaza na por -, el trim elimina el espacio
 
-casos<-casos[Región=="Metropolitana",]
+casos<-casos[Región=="Metropolitana",] #reduce la base, solo muestra los de la región metropolitana
 
-library(ggplot2)
+library(ggplot2) #carga gg plot, graficos
 
 ggplot(casos[order(Edad,decreasing = T)],)+geom_bar(stat = 'identity' ,aes(x=`Centro de salud`, y=Edad/Edad, group=Sexo, fill=Edad)) + coord_flip()+ facet_wrap(~Sexo) 
 
--- Class_03/Script_Clase03.R
+++ Class_03/Script_Clase03.R
@@ -9,24 +9,24 @@
 
 # Reading an exporting data
 
-library(readxl)
-library(data.table)
+library(readxl) #Cargamos lector de excel
+library(data.table) #cargamos data table
 
-casos<-data.table(read_excel("~/Documents/MBAn/Software tools for Big Data/Spatial Analytics/Clases/Class_02/2020-03-17-Casos-confirmados.xlsx",na = "—",trim_ws = TRUE,col_names = TRUE),stringsAsFactors = FALSE)
+casos<-data.table(read_excel("~/Desktop/MBAn/Software tools for Big Data/Spatial Analytics/Clases/Class_02/2020-03-17-Casos-confirmados.xlsx",na = "—",trim_ws = TRUE,col_names = TRUE),stringsAsFactors = FALSE) #dato table del excel, los - del excel toman valor NA, las columnas toman nombre del excel por el true
 
-casos<-casos[Región=="Metropolitana",]
+casos<-casos[Región=="Metropolitana",] #tomamos la data table y lo filtramos por casos, región metropolitana
 
-saveRDS(casos,"~/Documents/MBAn/Software tools for Big Data/Spatial Analytics/Clases/Class_03/CasosCovid_RM.csv") #guarda
+saveRDS(casos,"~/Desktop/MBAn/Software tools for Big Data/Spatial Analytics/Clases/Class_03/CasosCovid_RM.csv") #guarda un archivo de R
 
-write.csv(casos,file = '~/Documents/MBAn/Software tools for Big Data/Spatial Analytics/Clases/Class_03/CasosCovid_RM.csv',fileEncoding = 'UTF-8')
+write.csv(casos,file = '~/Desktop/MBAn/Software tools for Big Data/Spatial Analytics/Clases/Class_03/CasosCovid_RM.csv',fileEncoding = 'UTF-8') #guarda como csv
 
 writexl::write_xlsx
 
-library(foreign)
+library(foreign) #carga foreign
 
 write.dta
 
-casosRM<-fread("~/Documents/MBAn/Software tools for Big Data/Spatial Analytics/Clases/Class_03/CasosCovid_RM.csv",header = T, showProgress = T,data.table = T)
+casosRM<-fread("~/Desktop/MBAn/Software tools for Big Data/Spatial Analytics/Clases/Class_03/CasosCovid_RM.csv",header = T, showProgress = T,data.table = T)
 
 casosRM[,table(Sexo)]
 casosRM[Sexo=="Fememino",Sexo:="Femenino"]
@@ -34,59 +34,60 @@ casosRM[Sexo=="Fememino",Sexo:="Femenino"]
 
 # Creating (factor) variables
 
-class(casosRM$Sexo)
+class(casosRM$Sexo) #busca la clase de la variable sexo en los casos RM
 
 casosRM[,Sexo:=factor(Sexo,nmax = 2)]
 
-head(casosRM$Sexo)
-head(as.numeric(casosRM$Sexo))
+head(casosRM$Sexo) #me da los primeros 6 para la variable que pedimos
+head(as.numeric(casosRM$Sexo)) #me da los primeros 6 para la variable sexo como numero, 1 mujer, 2 hombre
 
-table(casosRM$Sexo)
-casosRM[,.N,by=.(Sexo)]
-casosRM[,.N,by=.(Sexo,`Centro de salud`)]
+table(casosRM$Sexo) #contador de tabla para casos RM, por sexo
+casosRM[,.N,by=.(Sexo)] #para casos RM, dime el numero para la variable sexo
+casosRM[,.N,by=.(Sexo,`Centro de salud`)] #para casos RM, dime el numero para el sexo segun centro de salud
 
 #Collapsing by Centro de Salud 
 
-casosRM[,sum(`Casos confirmados`,na.rm = T),by=.(`Centro de salud`)][,V1/sum(V1)]
+casosRM[,sum(`Casos confirmados`,na.rm = T),by=.(`Centro de salud`)][,V1/sum(V1)] #para casosRM remueve los NA, suma los casos confirmados por centro de salud y en base a eso, v1/sum(v1) donde v1 es el numero por centor de salud y sum el total 152 
 
 # collapsing by average age
 
-A<-casosRM[,.(AvAge=mean(Edad,na.rm = T)),by=.(`Centro de salud`)]
+A<-casosRM[,.(AvAge=mean(Edad,na.rm = T)),by=.(`Centro de salud`)] #A nueva tabla con casos de Rm, con el promedio de la edad, removiendo NA, para cada centro de salud
 
-B<-casosRM[,.(Total_centro=sum(`Casos confirmados`,na.rm = T)),by=.(`Centro de salud`)]
+B<-casosRM[,.(Total_centro=sum(`Casos confirmados`,na.rm = T)),by=.(`Centro de salud`)] #B nueva tabla con casos de Rm, con la suma de los casos confirmados removiendo NA, para cada centro de salud 
 
-C<-casosRM[Sexo=="Femenino",.(Total_Centro_Mujeres=sum(`Casos confirmados`,na.rm = T)),by=.(`Centro de salud`)]
+C<-casosRM[Sexo=="Femenino",.(Total_Centro_Mujeres=sum(`Casos confirmados`,na.rm = T)),by=.(`Centro de salud`)]#C nueva tabla para casos de Rm, con la suma de las mujeres confirmadas por centro de salud
 
-D<-casosRM[Sexo=="Masculino",.(Total_Centro_Hombres=sum(`Casos confirmados`,na.rm = T)),by=.(`Centro de salud`)]
+D<-casosRM[Sexo=="Masculino",.(Total_Centro_Hombres=sum(`Casos confirmados`,na.rm = T)),by=.(`Centro de salud`)]#D nueva tabla con casos de Rm, con la suma de los hombres confirmados por centro de salud
 
 
 #merging data sets
 
-AB<-merge(A,B,by = "Centro de salud",all = T,sort = F)
-ABC<-merge(AB,C,by = "Centro de salud",all = T,sort = F)
-ABCD<-merge(ABC,D,by = "Centro de salud",all = T,sort = F)
+AB<-merge(A,B,by = "Centro de salud",all = T,sort = F) #Une A, con B en una sola tabla guiada por centro de salud, all T, no llena con datos x
+ABC<-merge(AB,C,by = "Centro de salud",all = T,sort = F) #Une AB, con C en una sola tabla guiada por centro de salud, all T, no llena con datos x
+ABCD<-merge(ABC,D,by = "Centro de salud",all = T,sort = F) #Une ABC, con D en una sola tabla guiada por centro de salud, all T, no llena con datos x
 
-ABCD[,porc_mujeres:=Total_Centro_Mujeres/Total_centro]
+ABCD[,porc_mujeres:=Total_Centro_Mujeres/Total_centro] #CREA una nueva variable con el := que es el porcentaje de mujeres
 
 # reshaping
 
-E<-casosRM[,.(AvAge=mean(Edad,na.rm = T),`Casos confirmados`=sum(`Casos confirmados`,na.rm = T)),by=.(`Centro de salud`,Sexo)]
-G<-reshape(E,direction = 'wide',timevar = 'Sexo',v.names = c('AvAge','Casos confirmados'),idvar = 'Centro de salud')
+E<-casosRM[,.(AvAge=mean(Edad,na.rm = T),`Casos confirmados`=sum(`Casos confirmados`,na.rm = T)),by=.(`Centro de salud`,Sexo)]#casos RM, les saca el promedio por edad, y suma los casos confirmados ordenando por centro de salud y sexo
+G<-reshape(E,direction = 'wide',timevar = 'Sexo',v.names = c('AvAge','Casos confirmados'),idvar = 'Centro de salud') #separa por centro de salud, los casos confirmados y el promedio por sexo correspondiente
 
 #---- Part 2: Visualization  -------------------
 
 #Scatter plot
   #Base R 
-plot(G$`Casos confirmados.Femenino`,G$`Casos confirmados.Masculino`)
-text(x =G$`Casos confirmados.Femenino`,y=G$`Casos confirmados.Masculino`, G$`Centro de salud`,cex=0.5)
+plot(G$`Casos confirmados.Femenino`,G$`Casos confirmados.Masculino`) #abre un plot de G, respecto a los casos confirmados por sexo según eje
+text(x =G$`Casos confirmados.Femenino`,y=G$`Casos confirmados.Masculino`, G$`Centro de salud`,cex=0.5) #le agrega al plot los textos según eje, con tamaño 0,5 el nombre de cada clinica que es lo último
 
 #ggplot2
-p1<-ggplot(G,aes(x=`Casos confirmados.Femenino`,y=`Casos confirmados.Masculino`))+geom_point(aes(size=AvAge.Femenino,colour=AvAge.Masculino))+geom_text(aes(label=`Centro de salud`),size=2,check_overlap = T)
+library(ggplot2)
+p1<-ggplot(G,aes(x=`Casos confirmados.Femenino`,y=`Casos confirmados.Masculino`))+geom_point(aes(size=AvAge.Femenino,colour=AvAge.Masculino))+geom_text(aes(label=`Centro de salud`),size=2,check_overlap = T) #nuevo grafico según sexo para cada clínica
 p1
 
 #plotly
 library(plotly)
-ggplotly(p1)
+ggplotly(p1) #una forma más linda de ver lo anterior
 
 # other useful ways to show data
 
@@ -100,17 +101,17 @@ library(chilemapas)
 library(data.table)
 library(ggplot2)
 
-zonas_censo<-data.table(censo_2017_zonas,stringsAsFactors = F)
+zonas_censo<-data.table(censo_2017_zonas,stringsAsFactors = F) #traemos la tabla para trabajarla con ese nombre de zonas_censo
 
-poblacion_adulto_mayor_zonas<-zonas_censo[edad=="65 y mas",.(AdultosMayores=sum(poblacion)),by=.(geocodigo)]
+poblacion_adulto_mayor_zonas<-zonas_censo[edad=="65 y mas",.(AdultosMayores=sum(poblacion)),by=.(geocodigo)] #filtramos adultos mayores de 65 para el censo según población y codico
 
-zonas_valparaiso<-mapa_zonas[mapa_zonas$codigo_region=="05",]
+zonas_valparaiso<-mapa_zonas[mapa_zonas$codigo_region=="05",] #nos ve las zonas y el codigo de región 05, que es valparaiso y nos da la info
 
-zonas_valparaiso<-merge(zonas_valparaiso,codigos_territoriales[,c("codigo_comuna","nombre_comuna")],by="codigo_comuna",all.x=TRUE,sort=F)
+zonas_valparaiso<-merge(zonas_valparaiso,codigos_territoriales[,c("codigo_comuna","nombre_comuna")],by="codigo_comuna",all.x=TRUE,sort=F)#merge zonas de valparaiso con su comuna respectiva y su codigo respectivo
 
-zonas_valparaiso<-zonas_valparaiso[zonas_valparaiso$codigo_comuna%in%c("05101","05109"),]
+zonas_valparaiso<-zonas_valparaiso[zonas_valparaiso$codigo_comuna%in%c("05101","05109"),] #filtra para esos codigos de comuna
 
-zonas_valparaiso<-merge(zonas_valparaiso,poblacion_adulto_mayor_zonas,by="geocodigo",all.x=TRUE,sort=F)
+zonas_valparaiso<-merge(zonas_valparaiso,poblacion_adulto_mayor_zonas,by="geocodigo",all.x=TRUE,sort=F) #cuenta los adultos mayores y los une para el geocodigo
 
 
 #plotting
@@ -118,7 +119,7 @@ library(RColorBrewer)
 paleta <- rev(brewer.pal(n = 9,name = "Reds"))
 
 
-ggplot(zonas_valparaiso) + 
+ggplot(zonas_valparaiso) +  #un mapa gigante de plot, sobre la población de adulto mayor
   geom_sf(aes(fill = AdultosMayores, geometry = geometry)) +
   scale_fill_gradientn(colours = rev(paleta), name = "Poblacion\nadulto mayor") +
   labs(title = "Poblacion de 65 años y más", subtitle = "Valparaíso y Viña del Mar") +
@@ -128,7 +129,7 @@ ggplot(zonas_valparaiso) +
 zonas_valparaiso2<-cbind(zonas_valparaiso[,c("geocodigo","codigo_comuna","codigo_provincia","codigo_region","geometry")],"AdultosMayores"=sample(zonas_valparaiso$AdultosMayores,size = length(zonas_valparaiso$AdultosMayores)))
 
 
-ggplot(zonas_valparaiso2) + 
+ggplot(zonas_valparaiso2) + #mismo para para el codigo anterior, el cual queda mucho más bonito y contempla todo
   geom_sf(aes(fill = AdultosMayores, geometry = geometry)) +
   scale_fill_gradientn(colours = rev(paleta), name = "Poblacion\nadulto mayor") +
   labs(title = "Poblacion de 65 años y más", subtitle = "Valparaíso y Viña del Mar") +
@@ -136,6 +137,6 @@ ggplot(zonas_valparaiso2) +
 
 #comparing histograms of the same variable
 
-hist(zonas_valparaiso$AdultosMayores,main = "Histograma Adultos Mayores Viña-Valpo")
+hist(zonas_valparaiso$AdultosMayores,main = "Histograma Adultos Mayores Viña-Valpo")#muestra que ambos son iguales en un histograma de adultos mayores
 
 hist(zonas_valparaiso2$AdultosMayores,main = "Histograma Adultos Mayores Viña-Valpo")
